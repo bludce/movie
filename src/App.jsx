3ccd059efc247544ad2529cb2bac7aeaf95c48e3
Aleksey Zadorozhny <alekszadorozhny90@gmail.com>
2020-06-01 10:52:04 +0300

    feat: add basic routes

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import './index.sass';
+
+class App extends Component {
+
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <header></header>
+          <div className="App-main">
+            <div className="App-sidebar-wrapper">
+              {/* sidebar */}
+              {/* footer */}
+            </div>
+              <div className="App-content-wrapper">
+                <Route exact path="/login"  />
+                <Route exact path="/logout"  />
+                <Route exact path="/" />
+                <Route exact path="/popular" />
+                <Route exact path="/top-rated" />
+                <Route exact path="/coming-soon" />
+                <Route path="/search" />
+                <Route exact path="/favorites" />
+
+                <Route exact path="/watch-later" />
+              </div>
+          </div>
+        </div>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default App
